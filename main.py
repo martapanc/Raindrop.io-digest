909fab8d1fa78670a3864392573ccd3a20282570
Marta Pancaldi <marta.panc@gmail.com>
2020-08-25 23:36:03 +0100

    Add method to obtain auth code

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
+import os
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+
 from dotenv import load_dotenv
 from pathlib import Path
 from selenium import webdriver
 
 # Setup functions to read from .env file
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
+
+BASE_URL = "https://raindrop.io/oauth/"
+AUTH = "{}authorize?redirect_uri=http://localhost:5000&client_id={}".format(BASE_URL, os.getenv('RAINDROP_CLIENT_ID'))
 
 
 def obtain_token():
     driver = webdriver.Chrome('./chromedriver')
-    driver.get("https://raindrop.io/oauth/authorize?redirect_uri=http://localhost:5000&client_id=5f4573de41aff2d4a6774fa2")
-    button = driver.find_element_by_id('submit')
-    button.click()
-    print(driver.current_url)
-    pass
+    driver.get(AUTH)
+    delay = 3
+
+    try:
+        email_input = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, 'email')))
+        pw_input = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, 'password')))
+        login_button = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'loginButton')))
+
+        email_input.send_keys(os.getenv('RAINDROP_EMAIL'))
+        pw_input.send_keys(os.getenv('RAINDROP_PW'))
+        login_button.click()
+
+        button = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '//input[@type="submit" and @value="Agree"]')))
+        button.click()
+        url = urlparse.urlparse(driver.current_url)
+        param = parse_qs(url.query)['code']
+        print(param)
+        pass
+
+    except TimeoutError:
+        print("Page took too long to load")
 
 
 if __name__ == '__main__':
