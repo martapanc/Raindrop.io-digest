74be958196900f86db1e45a1a0cc9ff99b26bce9
Marta Pancaldi <marta.panc@gmail.com>
2020-09-10 22:24:19 +0100

    Rename Flask app

--- api.py
+++ api.py
@@ -8,9 +8,9 @@ from flask_httpauth import HTTPTokenAuth
 
 from main import get_random_bookmarks, get_random_bookmarks_in_last_days
 
-app = Flask(__name__)
+api = Flask(__name__)
 auth = HTTPTokenAuth(scheme='Bearer')
-CORS(app)
+CORS(api)
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -31,29 +31,29 @@ def auth_error(status):
     return jsonify({"error": status, "message": "Unauthorized"})
 
 
-@app.route('/health')
+@api.route('/health')
 def hello_world():
     return jsonify({"server status": "UP"})
 
 
-@app.route('/latest', methods=['POST'])
+@api.route('/latest', methods=['POST'])
 @auth.login_required
 def latest_raindrops():
 
     return jsonify({"message": "hello world", "body": request.get_json()})
 
 
-@app.route('/random/<number>', methods=['GET'])
+@api.route('/random/<number>', methods=['GET'])
 @auth.login_required
 def random_raindrops(number):
     return jsonify(get_random_bookmarks(int(number)))
 
 
-@app.route('/random/<number>/days/<days>', methods=['GET'])
+@api.route('/random/<number>/days/<days>', methods=['GET'])
 @auth.login_required
 def random_recent_raindrops(number, days):
     return jsonify(get_random_bookmarks_in_last_days(int(number), int(days)))
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    api.run(port=5000)
